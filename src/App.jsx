a0268f22675ee4d391e5f713a6fcd8050965e046
davickreations <davickreations2@gmail.com>
2024-07-10 11:54:25 +0100

    basename added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/belle-app">
     <Header />
     <Routes>
       <Route path='/' element={<ProductListing />}/>
